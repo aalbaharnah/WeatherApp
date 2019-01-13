a3e50f7298062f1434ec6051b116dc6b0db01001
Ali Albaharnah <aalbaharnah1@gmail.com>
2019-01-13 16:56:00 +0300

    third commit

--- platforms/android/app/build/intermediates/assets/debug/www/js/index.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -1,6 +1,6 @@
 var app = angular.module("MyWeatherApp", ['ngRoute']);
 
-
+// Routing to the main body page.
 app.config(function ($routeProvider) {
     $routeProvider
     .when('/',
@@ -10,10 +10,26 @@ app.config(function ($routeProvider) {
     })
 });
 
+
+
 app.controller("MyWeatherController", function ($scope, $http) {
 
-    localStorage.setItem('name', 'qatif');
+    localStorage.setItem('name', 'qatif,sa');
     console.log(localStorage.getItem('name'));
+
+    $scope.getMeHome = function () {
+        $http.get('http://api.apixu.com/v1/current.json?key=46114ce99ca14871b8963021191301&q=' + localStorage.getItem('name')).then(function(response){
+        console.log(response.data);    
+        $scope.cityData = response.data.location.name;
+        $scope.cityTemp = response.data.current.temp_c + '°';
+        $scope.cityWind = response.data.current.wind_kph +' kph';
+        $scope.cityHumidity = response.data.current.humidity + '%';
+        $scope.cityIcon = response.data.current.condition.icon;
+        $scope.cityCondition = response.data.current.condition.text;
+        $scope.cityDay =  response.data.current.is_day;
+        });
+    }
+    
 
     // Function to search by city name.
     $scope.cityName = '';
@@ -34,20 +50,18 @@ app.controller("MyWeatherController", function ($scope, $http) {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((function(position){
             console.log(position.coords.longitude.toFixed(6) + '    ' + position.coords.latitude.toFixed(6));
-            
+
             // Using Google API.
             $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng='+position.coords.latitude.toFixed(6)+','+position.coords.longitude.toFixed(6)+'&key=AIzaSyDzgPZGicdxjiHLFVem3lDIxR_XAVSEZus').then(function(loc){
                 console.log(loc.data.results);
-                
-                for (var i = 0; i < loc.data.results.length; i++){  
+                for (var i = 0; i < loc.data.results.length; i++){
                     //console.log(loc.data.results[i].address_components[0].types[0]);
                     if ( loc.data.results[i].address_components[0].types[0] == "locality") {
                         console.log(loc.data.results[i].address_components[0].long_name);
                         $scope.cityName = loc.data.results[i].address_components[0].long_name;
                     }
                 }
-
-                $http.get('http://api.apixu.com/v1/current.json?key=46114ce99ca14871b8963021191301&q=' + $scope.cityName).then(function(response){   
+                $http.get('http://api.apixu.com/v1/current.json?key=46114ce99ca14871b8963021191301&q=' + $scope.cityName).then(function(response){
                     $scope.cityData = response.data.location.name;
                     $scope.cityTemp = response.data.current.temp_c + '°';
                     $scope.cityWind = response.data.current.wind_kph +' kph';
@@ -55,14 +69,14 @@ app.controller("MyWeatherController", function ($scope, $http) {
                     $scope.cityIcon = response.data.current.condition.icon;
                     $scope.cityCondition = response.data.current.condition.text;
                 });
-
+ 
             });
-            
+ 
         }));
         console.log("yes")
     } else {
         console.log("geolocation is not supporter by this browser.");
-    }  
+    } 
 
     // Function to transition the backgroun based on the timing of the city.
     $scope.is_it_day = function () {
@@ -88,11 +102,6 @@ app.controller("MyWeatherController", function ($scope, $http) {
         "font-weight": "900",
         "font-size": "9vw",
         "letter-spacing": ".1rem",
-        "backface-visibility": "hidden",
-        "-webkit-transition": "content 2s ease-out",
-        "-moz-transition": "content 2s ease-out",
-        "-o-transition": "content 2s ease-out",
-        "transition": "content 2s ease-out"
     }
 
     $scope.CityTempStyle = {
--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,6 @@
 var app = angular.module("MyWeatherApp", ['ngRoute']);
 
-
+// Routing to the main body page.
 app.config(function ($routeProvider) {
     $routeProvider
     .when('/',
@@ -10,10 +10,26 @@ app.config(function ($routeProvider) {
     })
 });
 
+
+
 app.controller("MyWeatherController", function ($scope, $http) {
 
-    localStorage.setItem('name', 'qatif');
+    localStorage.setItem('name', 'qatif,sa');
     console.log(localStorage.getItem('name'));
+
+    $scope.getMeHome = function () {
+        $http.get('http://api.apixu.com/v1/current.json?key=46114ce99ca14871b8963021191301&q=' + localStorage.getItem('name')).then(function(response){
+        console.log(response.data);    
+        $scope.cityData = response.data.location.name;
+        $scope.cityTemp = response.data.current.temp_c + '°';
+        $scope.cityWind = response.data.current.wind_kph +' kph';
+        $scope.cityHumidity = response.data.current.humidity + '%';
+        $scope.cityIcon = response.data.current.condition.icon;
+        $scope.cityCondition = response.data.current.condition.text;
+        $scope.cityDay =  response.data.current.is_day;
+        });
+    }
+    
 
     // Function to search by city name.
     $scope.cityName = '';
@@ -34,12 +50,10 @@ app.controller("MyWeatherController", function ($scope, $http) {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((function(position){
             console.log(position.coords.longitude.toFixed(6) + '    ' + position.coords.latitude.toFixed(6));
- 
- 
+
             // Using Google API.
             $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng='+position.coords.latitude.toFixed(6)+','+position.coords.longitude.toFixed(6)+'&key=AIzaSyDzgPZGicdxjiHLFVem3lDIxR_XAVSEZus').then(function(loc){
                 console.log(loc.data.results);
- 
                 for (var i = 0; i < loc.data.results.length; i++){
                     //console.log(loc.data.results[i].address_components[0].types[0]);
                     if ( loc.data.results[i].address_components[0].types[0] == "locality") {
@@ -47,7 +61,6 @@ app.controller("MyWeatherController", function ($scope, $http) {
                         $scope.cityName = loc.data.results[i].address_components[0].long_name;
                     }
                 }
- 
                 $http.get('http://api.apixu.com/v1/current.json?key=46114ce99ca14871b8963021191301&q=' + $scope.cityName).then(function(response){
                     $scope.cityData = response.data.location.name;
                     $scope.cityTemp = response.data.current.temp_c + '°';
