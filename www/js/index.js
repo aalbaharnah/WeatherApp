670ecf63287c2b81dcd6458bfae6fa6d9290ed84
Ali Albaharnah <aalbaharnah1@gmail.com>
2019-01-14 11:25:12 +0300

    Monday commit

--- www/js/index.js
+++ www/js/index.js
@@ -3,92 +3,94 @@ var app = angular.module("MyWeatherApp", ['ngRoute', 'ngAnimate']);
 // Routing to the main body page.
 app.config(function ($routeProvider) {
     $routeProvider
-    .when('/',
-    {
-        controller: 'MyWeatherController',
-        templateUrl: 'templates/data.html'
-    })
+        .when('/', {
+            controller: 'MyWeatherController',
+            templateUrl: 'templates/data.html'
+        })
+        .when('/search', {
+            controller: 'MyWeatherController',
+            templateUrl: 'templates/search.html'
+        })
 });
 
-app.controller("MyWeatherController", function ($scope, $http) {
+app.controller("MyWeatherController", function ($scope, $http, $timeout) {
 
     // Setting Local Storage.
     localStorage.setItem('name', 'qatif,sa');
     console.log(localStorage.getItem('name'));
 
-    // Function to set the variables according to the local storage.
-    $scope.getMeHome = function () {
-        $http.get('http://api.apixu.com/v1/current.json?key=46114ce99ca14871b8963021191301&q=' + localStorage.getItem('name'))
-        .then(function(response){
-            console.log(response.data);    
-            $scope.cityData = response.data.location.name;
-            $scope.cityTemp = response.data.current.temp_c + '°';
-            $scope.cityWind = response.data.current.wind_kph +' kph';
-            $scope.cityHumidity = response.data.current.humidity + '%';
-            $scope.cityIcon = response.data.current.condition.icon;
-            $scope.cityCondition = response.data.current.condition.text;
-            $scope.cityDay =  response.data.current.is_day;
+    $scope.cities = ['saihat', 'london', localStorage.getItem('name')];
+    $scope.addCity = function () {
+        $scope.cities.push($scope.addIt);
+    }
+    $scope.removeCity = function (x) {
+        $scope.cities.splice(x, 1);
+    }
+
+    $scope.loading = false;
+
+    // Function to get the city name data.
+    $scope.getAllData = function (name) {
+        $http.get('http://api.apixu.com/v1/current.json?key=46114ce99ca14871b8963021191301&q=' + name)
+            .then(function (response) {
+                $scope.loading = true;
+                $timeout(function () {
+                    $scope.loading = false;
+                }, 1100)
+                console.log(response.data);
+                $scope.cityData = response.data.location.name;
+                $scope.cityTemp = response.data.current.temp_c + '°';
+                $scope.cityWind = response.data.current.wind_kph + ' kph';
+                $scope.cityHumidity = response.data.current.humidity + '%';
+                $scope.cityIcon = response.data.current.condition.icon;
+                $scope.cityCondition = response.data.current.condition.text;
+                $scope.cityDay = response.data.current.is_day;
         });
     }
-    
+
+    // Function to set the variables according to the local storage.
+    $scope.getMeHome = function () {
+        $scope.getAllData(localStorage.getItem('name'));
+    }
+
     // Function to search by city name.
     $scope.cityName = '';
     $scope.search = function () {
-        $http.get('http://api.apixu.com/v1/current.json?key=46114ce99ca14871b8963021191301&q=' + $scope.cityName)
-        .then(function(response){
-            console.log(response.data);    
-            $scope.cityData = response.data.location.name;
-            $scope.cityTemp = response.data.current.temp_c + '°';
-            $scope.cityWind = response.data.current.wind_kph +' kph';
-            $scope.cityHumidity = response.data.current.humidity + '%';
-            $scope.cityIcon = response.data.current.condition.icon;
-            $scope.cityCondition = response.data.current.condition.text;
-            $scope.cityDay =  response.data.current.is_day;
-        });
+        $scope.getAllData($scope.cityName);
     }
 
     // Function to get user current location.
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((function(position){
+        navigator.geolocation.getCurrentPosition((function (position) {
             console.log(position.coords.longitude.toFixed(6) + '    ' + position.coords.latitude.toFixed(6));
 
             // Using Google API.
-            $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng='+position.coords.latitude.toFixed(6)+','+position.coords.longitude.toFixed(6)+'&key=AIzaSyDzgPZGicdxjiHLFVem3lDIxR_XAVSEZus')
-            .then(function(loc){
-                console.log(loc.data.results);
-                for (var i = 0; i < loc.data.results.length; i++){
-                    //console.log(loc.data.results[i].address_components[0].types[0]);
-                    if ( loc.data.results[i].address_components[0].types[0] == "locality") {
-                        console.log(loc.data.results[i].address_components[0].long_name);
-                        $scope.cityName = loc.data.results[i].address_components[0].long_name;
+            $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude.toFixed(6) + ',' + position.coords.longitude.toFixed(6) + '&key=AIzaSyDzgPZGicdxjiHLFVem3lDIxR_XAVSEZus')
+                .then(function (loc) {
+                    console.log(loc.data.results);
+                    for (var i = 0; i < loc.data.results.length; i++) {
+                        //console.log(loc.data.results[i].address_components[0].types[0]);
+                        if (loc.data.results[i].address_components[0].types[0] == "locality") {
+                            console.log(loc.data.results[i].address_components[0].long_name);
+                            $scope.cityName = loc.data.results[i].address_components[0].long_name;
+                        }
                     }
-                }
-                $http.get('http://api.apixu.com/v1/current.json?key=46114ce99ca14871b8963021191301&q=' + $scope.cityName)
-                .then(function(response){
-                    $scope.cityData = response.data.location.name;
-                    $scope.cityTemp = response.data.current.temp_c + '°';
-                    $scope.cityWind = response.data.current.wind_kph +' kph';
-                    $scope.cityHumidity = response.data.current.humidity + '%';
-                    $scope.cityIcon = response.data.current.condition.icon;
-                    $scope.cityCondition = response.data.current.condition.text;
+                    $scope.getAllData($scope.cityName);
                 });
- 
-            });
- 
         }));
         console.log("yes")
     } else {
         console.log("geolocation is not supporter by this browser.");
-    } 
+    }
 
     // Function to transition the backgroun based on the timing of the city.
     $scope.is_it_day = function () {
         if ($scope.cityDay === 0) {
             console.log("is is night there");
             return {
-                "background-color" : "#ffc7ba",
+                "background-color": "#ffc7ba",
                 "-webkit-transition": "background-color 2s ease-out",
-                "-moz-transition":" background-color 2s ease-out",
+                "-moz-transition": " background-color 2s ease-out",
                 "-o-transition": "background-color 2s ease-out",
                 "transition": "background-color 2s ease-out"
             };
