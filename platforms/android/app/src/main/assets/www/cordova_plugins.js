a4be0f85f1463c84eaaed9c70754059330b3239c
aalbaharnah <baharnah.13@gmail.com>
2019-01-19 13:24:20 +0300

    some changes

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -15,6 +15,14 @@ module.exports = [
     "runs": true
   },
   {
+    "id": "cordova-plugin-native-keyboard.NativeKeyboard",
+    "file": "plugins/cordova-plugin-native-keyboard/www/NativeKeyboard.js",
+    "pluginId": "cordova-plugin-native-keyboard",
+    "clobbers": [
+      "window.NativeKeyboard"
+    ]
+  },
+  {
     "id": "es6-promise-plugin.Promise",
     "file": "plugins/es6-promise-plugin/www/promise.js",
     "pluginId": "es6-promise-plugin",
@@ -33,6 +41,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-geolocation": "4.0.1",
+  "cordova-plugin-native-keyboard": "1.6.0",
   "cordova-plugin-whitelist": "1.3.3",
   "es6-promise-plugin": "4.2.2",
   "cordova-plugin-x-socialsharing": "5.4.4"
